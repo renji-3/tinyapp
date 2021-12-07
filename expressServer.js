db0bd2cf775416ee54a20c3eff65e158f89d876f
Ren Guidolin <8.renji@gmail.com>
2021-12-07 17:23:00 -0500

    delete button

--- expressServer.js
+++ expressServer.js
@@ -48,10 +48,13 @@ app.get("/hello", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  // console.log(req.params);
-  // console.log(longURL);
-  // console.log(urlDatabase);
   res.redirect(longURL);
+});
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
 });
 
 
