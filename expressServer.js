d0707211f35d7ff459bf9b666b95c6e715ee1d2b
Ren Guidolin <8.renji@gmail.com>
2021-12-06 23:34:54 -0500

    added partials bc missed earlier

--- expressServer.js
+++ expressServer.js
@@ -22,9 +22,12 @@ app.get("/urls", (req, res) => {
   res.render("urlsIndex", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  console.log(templateVars);
   res.render("urlsShow", templateVars);
 });
 
